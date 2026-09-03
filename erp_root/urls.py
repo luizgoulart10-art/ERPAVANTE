40d741965f22cd158fdbec4301f2cf59e9ca950f
Luiz Gustavo <luiz.goulart@avanteprojetos.com.br>
2026-09-03 17:10:21 -0300

    Ajustando rotas e redirecionamento inicial

--- erp_root/urls.py
+++ erp_root/urls.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('credit-cards/', include('credit_cards.urls')),
-    path('finance/', include('finance.urls')),
-    path('orders/', include('orders.urls')),
+    # Redireciona a página inicial (raiz) direto para o Admin do ERP:
+    path('', RedirectView.as_view(url='/admin/', permanent=False)),
 ]
